c4daa3efa55023f2f2544c4376b31599d51d8a7c
Daniel Leahy <daniel.leahy@ucd.ie>
2014-01-23 18:29:12 +0000

    put the working withings api setup into the withings api .java code
    this is the one i used successfully with

--- src/main/java/org/scribe/builder/api/WithingsApi.java
+++ src/main/java/org/scribe/builder/api/WithingsApi.java
@@ -1,38 +1,39 @@
 package org.scribe.builder.api;
 
-import org.scribe.model.*;
+import org.scribe.model.Token;
+import org.scribe.model.Verb;
 
-public class WithingsApi extends DefaultApi10a {
-	  private static final String AUTHORIZE_URL = "https://oauth.withings.com/account/authorize";
+public class WithingsApi extends DefaultApi10a
+{
+  private static final String AUTHORIZATION_URL = "https://oauth.withings.com/account/authorize";
+  
+  @Override
+  public String getAccessTokenEndpoint()
+  {
+    return "https://oauth.withings.com/account/access_token";
+  }
 
-	  @Override
-	  public String getAccessTokenEndpoint()
-	  {
-	    return "https://oauth.withings.com/account/access_token";
-	  }
+  @Override
+  public String getRequestTokenEndpoint()
+  {
+    return "https://oauth.withings.com/account/request_token";
+  }
+  
+  @Override
+  public String getAuthorizationUrl(Token requestToken)
+  {
+    return String.format(AUTHORIZATION_URL, requestToken.getToken());
+  }
+  
+  @Override
+  public Verb getRequestTokenVerb()
+  {
+    return Verb.GET;
+  }
 
-	  @Override
-	  public String getRequestTokenEndpoint()
-	  {
-	    return "https://oauth.withings.com/account/request_token";
-	  }
-	  
-	  @Override
-	  public String getAuthorizationUrl(Token requestToken)
-	  {
-	    return String.format(AUTHORIZE_URL, requestToken.getToken());
-	  }
-	  
-	  @Override
-	  public Verb getRequestTokenVerb()
-	  {
-	    return Verb.GET;
-	  }
-
-	  @Override
-	  public Verb getAccessTokenVerb()
-	  {
-	    return Verb.GET;
-	  }
-	  
-	}
+  @Override
+  public Verb getAccessTokenVerb()
+  {
+    return Verb.GET;
+  }  
+}
